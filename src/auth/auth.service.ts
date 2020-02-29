05338eea38b941f3da4fe6baf82a439afebfc348
dragan.mijatovic <dragan.mijatovic@cramick-it.com>
2020-02-29 13:28:28 +0100

    Basic SignUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -11,4 +12,7 @@ export class AuthService {
   ) {
   }
 
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
 }
