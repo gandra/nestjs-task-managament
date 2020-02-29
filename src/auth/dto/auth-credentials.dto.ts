19932b8018f822e73aed8e6291a4abf954b90841
dragan.mijatovic <dragan.mijatovic@cramick-it.com>
2020-02-29 14:22:15 +0100

    Username and password validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
-  password: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak.'
+  })
+  password: string; // https://gist.github.com/arielweinberger/18a29bfa17072444d45adaeeb8e92ddc
 }
